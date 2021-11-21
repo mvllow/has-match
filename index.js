25c1e68c9aac1aac635c194d5c73cc10c477aa85
mvllow <mvllow@icloud.com>
2021-11-20 20:30:22 -0600

    fix: filter array values without recursion

--- index.js
+++ index.js
@@ -21,7 +21,17 @@ export default function hasMatch(source, search, includeKeys) {
 		}
 
 		if (Array.isArray(value)) {
-			return hasMatch(value, search, includeKeys)
+			const result = value.map((item) => {
+				if (typeof item === 'string') {
+					const normalizedValue = item.normalize().toLowerCase()
+
+					return normalizedValue === normalizedSearch
+				}
+
+				return false
+			})
+
+			return result.includes(true)
 		}
 
 		return false
