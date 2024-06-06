bbdc2d810e2891f56c61583d81d888d928cf78af
mvllow <mvllow@icloud.com>
2024-06-06 12:36:09 -0500

    refactor: simplify code structure
    This commits bumps all dependencies to latest and formats with the
    latest prettier default settings (while still respecting the
    editorconfig).

--- index.js
+++ index.js
@@ -1,35 +1,25 @@
 export default function hasMatch(source, search, includeKeys) {
-	const normalizedSearch = search.normalize().toLowerCase()
+	const normalizedSearch = search.normalize().toLowerCase();
 
-	const result = Object.keys(source).map((key) => {
-		const value = source[key]
-
+	return Object.keys(source).some((key) => {
 		if (includeKeys?.length > 0 && !includeKeys.includes(key)) {
-			return false
+			return false;
 		}
 
-		if (typeof value === 'string') {
-			const normalizedValue = source[key].normalize().toLowerCase()
+		const value = source[key];
 
-			return normalizedValue.includes(normalizedSearch)
+		if (typeof value === "string") {
+			return value.normalize().toLowerCase().includes(normalizedSearch);
 		}
 
 		if (Array.isArray(value)) {
-			const result = value.map((item) => {
-				if (typeof item === 'string') {
-					const normalizedValue = item.normalize().toLowerCase()
-
-					return normalizedValue.includes(normalizedSearch)
-				}
-
-				return false
-			})
-
-			return result.includes(true)
+			return value.some(
+				(item) =>
+					typeof item === "string" &&
+					item.normalize().toLowerCase().includes(normalizedSearch)
+			);
 		}
 
-		return false
-	})
-
-	return result.includes(true)
+		return false;
+	});
 }
